c0062caefbb96c55c913f22f5826dd5800b4cce4
Aniket Patra <iamaniketpatra@gmail.com>
2025-09-09 06:20:21 +0530

    unity fixes 2

--- app/unity/page.tsx
+++ app/unity/page.tsx
@@ -1,27 +1,9 @@
-// "use client";
-// export default function UnityDemo() {
-//   return (
-//     <div>
-//       <div className="w-full h-screen border-[#ff8906]">
-//         <iframe
-//           src="/unity/WebGLBuild/index.html"
-//           title="Unity WebGL"
-//           // width="1920px"
-//           // height="1080px"
-//           className="w-full h-full border-[#ff8906]"
-//           style={{ border: "none" }}
-//         ></iframe>
-//       </div>
-//     </div>
-//   );
-// }
-
 
 "use client";
 
-export default function UnityDemo() {
+export default function page() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-[#0f0e17] text-white p-6">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#0f0e17] text-white p-6 ">
       {/* Title */}
       <h1 className="text-3xl md:text-4xl font-bold mb-6 text-[#ff8906] drop-shadow-lg">
         Fire Escape
@@ -30,7 +12,7 @@ export default function UnityDemo() {
       {/* Game container */}
       <div className="w-full max-w-6xl aspect-video rounded-2xl overflow-hidden shadow-2xl border-4 border-[#ff8906] bg-black">
         <iframe
-          src="/unity/WebGLBuild/index.html"
+          src="/unity/WebGLBuild2/index.html"
           title="Unity WebGL"
           className="w-full h-full"
           style={{ border: "none" }}
